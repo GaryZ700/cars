adfdce851281714bedcfc2fb280565f93cadf1eb
root <root@DESKTOP-7TOTAEN.localdomain>
2018-12-04 13:07:26 -0800

    adds final cars.sh

--- cars.sh
+++ cars.sh
@@ -2,10 +2,6 @@
 #cars.sh
 #Gary Zeri
 
-#global variables defined here 
-#init loop control variable
-loop=false
-
 #############################################################################
 
 #All function defined here 
@@ -14,23 +10,29 @@ function mainMenu(){
 
 	#init loop control variable
 	loop=true
-	
+
+	#init message variable
+	message=""	
+
 	#loop to check user input to ensure it is valid
 	while($loop)
 	do		
 		#clear the screen and present the menu options to the user 
 		clear
+		echo $message
+		echo ""
 		echo "1. Add A Car"
 		echo "2. List Cars in the Inventory"
 		echo "3. Quit"
-		echo "\n Please choose and option: "
+		echo ""
+		echo "Please choose and option: "
+		echo ""
 	
-		read input 		
-		
-		result=0
-		loadOption input result		
+		read input
+ 		
+		loadOption $input result 
 
-		if [ loadOption input ]
+		if [ $result -eq 0 ]
 		then
 			loop=false
 		fi	
@@ -39,33 +41,73 @@ function mainMenu(){
 
 #############################################################################
 
-#function to load specifed option
-function loadOption(){
+#function to add a car to the list 
+function addCar(){
 	
-	case $1 in 
-		1 ) 
-			addCar()
-			returnValue=true;;
-		2 ) 
-			listCars()
-			returnValue=true;;
-		3 ) 
-			echo "Goodbye"
-			returnValue=2;;
-	esac
+	#clear the screen 
+	clear 
 	
-	return $returnValue
+	#ask for the year 
+	echo "Please enter the year of the vehicle kind sir or madam: "
+	read year
+
+	#ask for the make
+	echo "Please enter the make of your vehicle, Mr. or Mrs. Linux User: "
+	read make 
+	
+	#ask for the model 
+	echo "Last thing to ask! Can you please enter in the model of your vehicle all-mighty keyboard typer: "
+	read model	
+	
+	#create final string
+	echo "$year:$make:$model" >> My_old_cars
+
+	#create message string 
+	messageStr="Your $year $make $model was added to the list."
+	message=$messageStr
+}
+
+
+#############################################################################
+
+#function to dispalay the list of cars to the user
+function listCars(){
+
+	#use cat to get file, and then use tr to rmove the : characters form the fil
+	clear
+	echo ""
+        cat My_old_cars | tr ':' ' ' 
+	echo ""
+	echo "Press enter to return to the main menu."
+	read nothing
 }
 
 #############################################################################
 
-#init main while loop for the code 
-while $loop
-do
+#function to load specifed option
+function loadOption(){
 
-	#print the menu to the user
-	mainMenu()
+	#set up return value 	
+	local __return=$2
+	eval $__return=1
 
+	case $1 in 
+		"1" ) 
+			addCar
+			returnValue=true;;
+		"2" ) 
+			listCars
+			returnValue=true;;
+		"3" )
+			echo ""
+			echo "Goodbye"	
+			eval $__return="'0'";;
+		* )	
+			message="Invalid Option! Please choose another.";;
+	esac
+}
 
+#############################################################################
 
-done
+#start main loop of the program with the main menu 
+mainMenu
